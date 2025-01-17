ffcdf79f597fff47cad5e693c0647f431890a07f
SRDJAN\ksrdj <k.srdjan02@gmail.com>
2025-01-18 00:29:45 +0100

    CustomWindow dodan background color

--- KoleServis/KoleServis/obj/Debug/net8.0-windows/MVVM/View/Login.g.i.cs
+++ KoleServis/KoleServis/obj/Debug/net8.0-windows/MVVM/View/Login.g.i.cs
@@ -71,7 +71,7 @@ namespace KoleServis.MVVM.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/KoleServis;component/mvvm/view/login.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/KoleServis;V1.0.0.0;component/mvvm/view/login.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\MVVM\View\Login.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
